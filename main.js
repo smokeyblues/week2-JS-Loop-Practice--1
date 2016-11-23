8341e403c0d36799f7afa16bbf91f542cecb0e9a
smokeyblues <smokeyblues@hotmail.com>
2016-11-23 15:55:30 -0500

    finish exercises

---
+++ main.js
@@ -0,0 +1,70 @@
+for (var i = 0; i < 5; i++) {
+    console.log(i * 100)
+}
+
+for (var i = 1; i < 65; i++) {
+  if ( i === 1 || i < 5 && i % 2 === 0) {
+    console.log(i);
+  } if ( i > 5 && i % 5 !== 0 && i % 6 !== 0 && i % 7 !== 0 && i % 8 === 0) {
+    console.log(i);
+  }
+}
+
+// 1 2 4 8 16 32 64
+
+for (var i = 0; i < 3; i++) {
+  if (i === 0) {
+    console.log(1);
+    console.log(1);
+    console.log(1);
+  } if (i === 1) {
+    console.log(2); 
+    console.log(2);
+    console.log(2);
+  } if (i === 2) {
+    console.log(3);
+    console.log(3);
+    console.log(3);
+  }
+}
+
+// 1 1 1 2 2 2 3 3 3
+
+for (var i = 0; i < 11; i++) {
+  if (i % 2 === 0) {
+    console.log(i)
+  }
+}
+
+// 0 2 4 6 8 10
+
+for (var i = 1; i < 16; i++) {
+  if (i % 3 === 0) {
+      console.log(i)
+  }
+}
+
+// 3 6 9 12 15
+
+for (var i = 9; i > -1; i--) {
+    console.log(i)
+}
+
+// 9 8 7 6 5 4 3 2 1 0
+
+for (var i = 0; i < 3; i++) {
+    console.log(0);
+    console.log(1);
+    console.log(2);
+    console.log(3);
+}
+
+// 0 1 2 3 0 1 2 3 0 1 2 3
+
+
+
+
+
+
+
+
